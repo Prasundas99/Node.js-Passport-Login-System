625f8b7044db30f51fea2503258e8f883930bb17
Prasundas99 <prasundas115@gmail.com>
2022-12-08 12:22:23 +0530

    register login page

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index', { title: 'Home' });
     }
 );
+
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Login' });
+});
+
+app.post('/login', (req, res) => {
+});
+
+app.get('/register', (req, res) => {
+    res.render('register', { title: 'Register' });
+});
+
+app.post('/register', (req, res) => {});
+
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
