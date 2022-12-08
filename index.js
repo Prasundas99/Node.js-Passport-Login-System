f70e7aafafbe87fbd2edc9897d5da93e96bac5c8
Prasundas99 <prasundas115@gmail.com>
2022-12-08 12:30:11 +0530

    register functionality

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-import express from 'express';
+import express from "express";
+import bcrypt from "bcrypt";
 
 const app = express();
-app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
-    }
-);
+const users = [];
 
-app.get('/login', (req, res) => {
-    res.render('login', { title: 'Login' });
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
 });
 
-app.post('/login', (req, res) => {
+app.get("/login", (req, res) => {
+  res.render("login", { title: "Login" });
 });
 
-app.get('/register', (req, res) => {
-    res.render('register', { title: 'Register' });
+app.post("/login", (req, res) => {});
+
+app.get("/register", (req, res) => {
+  res.render("register", { title: "Register" });
 });
 
-app.post('/register', (req, res) => {});
-
-
-
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 69);
+    users.push({
+      id: Date.now().toString(),
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch (error) {
+    res.redirect("/register");
+  }
+  console.log(users);
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    }
-);
+  console.log("Server is running on port 3000");
+});
